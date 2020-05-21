102539338f72c6dd19c1830605d06ebd88116a4b
Ethan <ethan.parker13@gmail.com>
2020-05-21 13:43:03 +0100

    Who are you

---
+++ CSharp Test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharp_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wait a minite, who are you?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Well hello {0}.", name);
+        }
+    }
+}
